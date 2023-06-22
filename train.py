7cc106bbc86de51a3554b4ad00232928970193f8
selva <avirassk@hotmail.com>
2023-06-22 12:15:18 +0530

    pull

--- train.py
+++ train.py
@@ -6,16 +6,17 @@ import joblib
 import pickle
 
 if __name__ == "__main__":
-    training_data_directory = "/opt/ml/input/data/train"
+    training_data_directory = "/opt/ml/processing/train"
+    test_data_directory = "/opt/ml/processing/test"
     train_features_data = os.path.join(training_data_directory, "train_features.csv")
     train_labels_data = os.path.join(training_data_directory, "train_labels.csv")
     print("Reading input data")
-    #X_train = pd.read_csv(train_features_data, header=None)
-    #y_train = pd.read_csv(train_labels_data, header=None)
+    X_train = pd.read_csv(train_features_data, header=None)
+    y_train = pd.read_csv(train_labels_data, header=None)
 
     model = LogisticRegression(class_weight="balanced", solver="lbfgs")
     print("Training LR model")
-    #model.fit(X_train, y_train)
+    model.fit(X_train, y_train)
     #model_output_directory = os.path.join("/opt/ml/model", "model.joblib")
     #print("Saving model to {}".format(model_output_directory))
     #joblib.dump(model, model_output_directory)
