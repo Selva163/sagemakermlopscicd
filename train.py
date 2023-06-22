ab0656ffca6942e29e04cfc3114b381192282eb5
selva <avirassk@hotmail.com>
2023-06-22 19:37:52 +0530

    pull

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     training_data_directory = "/opt/ml/processing/train"
     test_data_directory = "/opt/ml/processing/test"
     train_features_data = ospath.join(training_data_directory, "train_features.csv")
-    train_labels_data = os.path.join.(training_data_directory, "train_labels.csv")
+    train_labels_data = os.path.join(training_data_directory, "train_labels.csv")
     print("Reading input data")
     X_train = pd.read_csv('s3://s3tmc101/train_features.csv', header=None)
     y_train = pd.read_csv('s3://s3tmc101/train_labels.csv', header=None)
