8045f7195a9bae07c2d5b74bb58441feb9ed0a5a
selva <avirassk@hotmail.com>
2023-06-24 23:55:34 +0530

    pull

--- pipeline_sample.py
+++ pipeline_sample.py
@@ -17,6 +17,7 @@ from sagemaker.workflow.steps import TrainingStep
 from sagemaker.workflow.pipeline import Pipeline
 from sagemaker.workflow.step_collections import RegisterModel
 import os 
+from sagemaker.workflow.pipeline_context import PipelineSession
 from sagemaker.workflow.conditions import ConditionGreaterThanOrEqualTo
 from sagemaker.workflow.condition_step import (
     ConditionStep
